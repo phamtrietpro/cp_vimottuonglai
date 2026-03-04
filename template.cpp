17bb89db2dc0a3a1c8b0fc1ef42ea3ea4b9a4923
Kduckp <ducphatkieu@gmail.com>
2026-03-04 16:17:26 +0700

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,1 +1,53 @@
+// Template Kieu Duc Phat 
+// * Nếu thích có thể lấy template này về chỉnh lại theo ý mình
+// #include<bits/stdc++.h>
 
+// using namespace std;
+
+// #define ll long long
+// #define pb push_back
+// #define all(x) x.begin() + 1, x.end()
+// #define nall(x) x.begin(), x.end()
+// #define vi vector<int>
+// #define vpii vector<pair<int,int>>
+// #define pll pair<ll, ll>
+// #define pii pair<int, int>
+// #define fi first
+// #define endl "\n"
+// #define se second
+// #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+// #define mem(a, x) memset(a, x, sizeof(a))
+// #define working cerr<<"If u see this message, your code is working"<<endl;
+
+// #define trailingzero(x) __builtin_ctzll(x)
+// #define cntbit1(x) __builtin_popcountll(x)
+// #define leadingzero(x) __builtin_clzll(x)
+
+// #define _______TOISETHIVOI_______ signed main()
+// #define KILL() exit(0)
+// #define NAME "duxp"
+
+// template <typename T>
+// inline int getbit(T x, int k) { return (x >> k) & 1; }
+// template <typename T>
+// inline T onbit(T x, int k) { return x | (T(1) << k); }
+// template <typename T>
+// inline T offbit(T x, int k) { return x & ~(T(1) << k); }
+
+// const ll OO = 1e18;
+// const int oo = 1e9;
+// const int MOD = 1000000007;
+// const int MOD2 = 998244353;
+// const int MAXN = 200000;
+
+//                                 _______TOISETHIVOI_______
+// {
+//     fast;
+//     if (FILE *f = fopen(NAME ".inp", "r")) {
+//         fclose(f);
+//         freopen(NAME ".inp", "r", stdin);
+//         freopen(NAME ".out", "w", stdout);
+//     }
+
+    
+// }
